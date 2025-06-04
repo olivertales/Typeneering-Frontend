fa31b39beebb923fc87121908a150ede4ede1c02
Thalles Oliveira <thalles.gbpereira@gmail.com>
2025-06-04 00:05:25 -0300

    Added interceptors and configured api url in environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/authInterceptor';
+import { generalInterceptor } from './core/interceptors/generalInterceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withInterceptors([authInterceptor, generalInterceptor]))
   ],
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: '127.0.0.1:5000'
+  apiUrl: 'http://localhost:5000'
 };
